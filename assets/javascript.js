21665b5b82cecc9d35f49c8abd1d868ba30acb43
Noah G <NoahBGlasser@gmail.com>
2019-03-01 21:55:32 -0800

    tweaked css

--- assets/javascript.js
+++ assets/javascript.js
@@ -65,6 +65,8 @@ function timeUp() {
     document.getElementById("grade").style.background = "grey";
     document.getElementById("grade").style.height = "20%";
     document.getElementById("grade").style.width = "95%";
+    document.getElementById("submit").style.background = "black%";
+    document.getElementById("time").style.background = "black%";
 }
 }
 
@@ -125,6 +127,8 @@ $(document).ready(function() {
         document.getElementById("grade").style.background = "grey";
         document.getElementById("grade").style.height = "20%";
         document.getElementById("grade").style.width = "95%";
+        document.getElementById("submit").style.background = "black%";
+        document.getElementById("time").style.background = "black%";
         
         }
     })
